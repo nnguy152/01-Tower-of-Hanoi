6b25273d52b26cbab632fa5d55a2a15bd4e73e9b
Nhi Nguyen <nnguy152@gmail.com>
2018-02-13 08:50:02 -0500

    added a bunch of functions

--- script.js
+++ script.js
@@ -9,40 +9,52 @@ var destination = null
 var start = null
 var clicked = true
 var clicked2 = true
+var movesStart = 0
+var moves = document.querySelector('.moves')
 
 // user clicks on starting container
 // stores container as start variable
 // stores firstChild in container as selected
 // turns clicked false so start won't be changed
 // calls selectDestination function
-if (clicked) {
-  container1.addEventListener('click', () => {
-    selected = container1.firstElementChild
-    console.log(container1.firstElementChild)
-    start = container1
-    console.log('selected start 1')
-    selectDestination()
-    clicked = false
-  })
-} else if (clicked) {
+document.onload = startGame()
+// @@@@@@@@ Why won't it keep clicked as false-> it changes
+// click back to true before executing the rest of the code
+function startGame () {
+  if (clicked === true) {
+    container1.addEventListener('click', () => {
+      selected = container1.firstElementChild
+      console.log(container1.firstElementChild)
+      start = container1
+      console.log('selected start 1')
+      clicked = false
+      selectDestination()
+    })
+  }
+}
+function startPart2 () {
+ if (clicked) {
   container2.addEventListener('click', () => {
     selected = container2.firstElementChild
-    console.log(container2.firstElementChild)
+      console.log(container2.firstElementChild)
     start = container2
-    console.log('selected start 2')
-    selectDestination()
+      console.log('selected start 2')
     clicked = false
+    selectDestination()
   })
-} else if (clicked) {
   container3.addEventListener('click', () => {
     selected = container3.firstElementChild
-    console.log(container3.firstElementChild)
+      console.log(container3.firstElementChild)
     start = container3
-    console.log('selected start 3')
-    selectDestination()
+      console.log('selected start 3')
     clicked = false
+    selectDestination()
   })
+ }
 }
+
+// @@@@@@@@@@@@@@@@@@@@@ can't execute other click start
+// possibilities without restarting entire operation
 
 // user picks 2nd container
 // stores as destination variable
@@ -65,135 +77,77 @@ function selectDestination () {
   } else if (start === container2) {
     container1.addEventListener('click', () => {
       destination = container1
-      console.log('selected destination')
       compare()
+      clicked2 = false
     })
     container3.addEventListener('click', () => {
       destination = container3
-      console.log('selected destination')
+      console.log('selected destination 3')
       compare()
+      clicked2 = false
     })
   } else if (start === container3) {
     container1.addEventListener('click', () => {
       destination = container1
-      console.log('selected destination')
+      console.log('selected destination 1')
       compare()
+      clicked2 = false
     })
     container2.addEventListener('click', () => {
       destination = container2
-      console.log('selected destination')
+      console.log('selected destination 2')
       compare()
+      clicked2 = false
     })
   }
 }
 
-// event prevent default?
+// remove Child doesn't work if not original parent container
+// says child node does not exist even though it is
+// visible in new container
+// Plus selected stays as last block clicked from original container
 function compare () {
   if (destination.hasChildNodes() === true) {
     if (selected.offsetWidth < destination.firstChild.offsetWidth) {
-      start.removeChild(selected)
-      destination.insertBefore(selected, destination.firstChild)
-      console.log('Yay it worked >_>')
+      addChild()
+      startPart2()
+    } else {
+      startGame()
+        console.log('no')
+        console.log(selected) //why it only saves the last child from original parent container
     }
   } else {
-    start.removeChild(selected)
-    console.log(start)
-    console.log(selected)
-    destination.appendChild(selected)
-    console.log(destination)
-    console.log('Yay it worked without children >_>')
+    addChildFirst()
+    startPart2()
   }
 }
 
-// container1.addEventListener('click', () => {
-//   selected = document.querySelector('.container').firstChild
-//   selectDestination()
-// })
+function addChild () {
+  start.removeChild(selected)
+  destination.prepend(selected)
+  console.log('Yay it worked >_>')
+  clicked = true
+  clicked2 = true
+  console.log(selected)
+  trackMoves()
+}
 
-// container2.addEventListener('click', () => {
-//   selected = document.querySelector('.container').firstChild
-//   selectDestination()
-// })
-// container3.addEventListener('click', () => {
-//   selected = document.querySelector('.container').firstChild
-//   selectDestination()
-// })
+function addChildFirst () {
+  start.removeChild(selected)
+      console.log(start)
+      console.log(selected)
+  destination.appendChild(selected)
+      console.log(destination)
+      console.log('Yay it worked without children >_>')
+  clicked = true
+  clicked2 = true
+  trackMoves()
+}
+
+function trackMoves () {
+  movesStart++
+  moves.innerHTML = `${movesStart}`
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function checkContainer(thisBlock) {
-//   if (container.contains(block1 || block2 || block3)) {
-//     if (thisblock )
-//   }
-// }
-// function checkWidth () {
-//   if 
-// }
-// function checkBlock () {
-//   if (container.contains(block) ) {
-//     console.log('hi')
-//   }
-// }
-// So basically.
-// Have container listen for dragover
-// when block dragged over
-// call check container function
-
-// function CHECK CONTAINER for what blocks it has => node.contain(othernode)
-// check for true or false
-// if container has small block, do not let mid or bottom block in on top
-// if container has middle block, only allow top block in on top
-// if container has bottom block, allow middle block in, allow top in
-// so if all conditions are true, allow MOVE function
-
-// function MOVE
-// move function -> allow whatever block clicked to insert itself on top
-
-// right now, these containers only allow top block to be dragged around
-
-// container[1].addEventListener('dragover', releaseBlock)
-
-// function releaseBlock (blockNum) {
-//   block[0].addEventListener('click', () => {
-//     bNum = blockNum
-//     console.log(bNum)
-//   })
-// }
-    // if (event.target.className === 'container') {
-    //   block[bNum].parentNode.removeChild(block[bNum])
-    //   container[bNum].insertBefore(block[bNum], container[cNum].firstChild)
-    // }
-
-  // container[1].addEventListener('dragover', (event) => {
-  //   event.preventDefault()
-  //   if (event.target.className === 'container two') {
-  //     block[0].parentNode.removeChild(block[0])
-  //     container[1].appendChild(block[0])
-  //   }
-  // })
-  // container[2].addEventListener('dragover', (event) => {
-  //   event.preventDefault()
-  //   if (event.target.className === 'container three') {
-  //     block[0].parentNode.removeChild(block[0])
-  //     container[2].appendChild(block[0])
-  //   }
-  // })
-
-// click and placing
-
-//To Do!
-//1. For each container - add event Listener, 'dragover'
-//2. 'dragover' calls function 'releaseBlock' with arguments for currentBlock and event
-//3. when block is clicked, store block num in global variable
-//4. when block released, pass in global block, and event.  
+// @@@@ WON'T ALLOW CONTAINER ONE TO BE CHOSEN
