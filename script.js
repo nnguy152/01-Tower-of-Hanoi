2050769436a8c733de532ff231ced4a181784cb5
Nhi Nguyen <nnguy152@gmail.com>
2018-02-15 09:06:48 -0500

    deleted old script.js file

--- script.js
+++ script.js
@@ -1,178 +1,279 @@
-var block1 = document.querySelector('.first')
-var block2 = document.querySelector('.second')
-var block3 = document.querySelector('.third')
+// selectors for containers/blocks
 var container1 = document.querySelector('.one')
 var container2 = document.querySelector('.two')
 var container3 = document.querySelector('.three')
+var block = document.querySelectorAll('.block')
+// stores first block on tower & first/second clicked container
 var selected = null
 var destination = null
 var start = null
-var clicked = true
-var clicked2 = true
+// stores total moves
 var movesStart = 0
 var moves = document.querySelector('.moves')
+// some silly code
+var winCounter = 0
+var button = document.querySelector('.button')
 
-// user clicks on starting container
-// stores container as start variable
-// stores firstChild in container as selected
-// turns clicked false so start won't be changed
-// calls selectDestination function
+// starts game upon page load
 document.onload = startGame()
-// @@@@@@@@ Why won't it keep clicked as false-> it changes
-// click back to true before executing the rest of the code
+
 function startGame () {
-  if (clicked === true) {
-    container1.addEventListener('click', () => {
-      selected = container1.firstElementChild
-      start = container1
-      console.log('selected start 1')
-      console.log(selected)
-      clicked = false
-      selectDestination()
-    })
+  addListeners()
+}
+
+// adds listeners to containers
+function addListeners () {
+  container1.addEventListener('click', containerOneClicked)
+  container2.addEventListener('click', containerTwoClicked)
+  container3.addEventListener('click', containerThreeClicked)
+}
+
+// when specific container clicked, goes to specific function
+// containerClicked function stores container/firstChild in variables declared at beginning
+// changes listener event functions to selectDestination
+function containerOneClicked () {
+  selected = container1.firstElementChild
+  start = container1
+  if (start.childElementCount === 0) {
+    startGame()
+  } else {
+    removeListener()
+    container2.addEventListener('click', selectDestination2)
+    container3.addEventListener('click', selectDestination3)
+  }
+}
+function containerTwoClicked () {
+  selected = container2.firstElementChild
+  start = container2
+  if (start.childElementCount === 0) {
+    startGame()
+  } else {
+    removeListener()
+    container1.addEventListener('click', selectDestination1)
+    container3.addEventListener('click', selectDestination3)
+  }
+}
+function containerThreeClicked () {
+  selected = container3.firstElementChild
+  start = container3
+  if (start.childElementCount === 0) {
+    startGame()
+  } else {
+    removeListener()
+    container1.addEventListener('click', selectDestination1)
+    container2.addEventListener('click', selectDestination2)
   }
 }
 
-function startPart2 () {
- if (clicked) {
-  container1.addEventListener('click', () => {
-    selected = container1.firstElementChild
-    start = container1
-    console.log('selected second start 1')
-    console.log(selected)
-    clicked = false
-    selectDestination()
-  })
-  container2.addEventListener('click', () => {
-    selected = container2.firstElementChild
-    start = container2
-      console.log('selected start 2')
-      console.log(selected)
-    clicked = false
-    selectDestination()
-  })
-  container3.addEventListener('click', () => {
-    selected = container3.firstElementChild
-    start = container3
-      console.log('selected start 3')
-      console.log(selected)
-    clicked = false
-    selectDestination()
-  })
- }
+// removes Listeners to prevent restarting from beginning
+function removeListener () {
+  container1.removeEventListener('click', containerOneClicked)
+  container2.removeEventListener('click', containerTwoClicked)
+  container3.removeEventListener('click', containerThreeClicked)
 }
 
-// @@@@@@@@@@@@@@@@@@@@@ can't execute other click start
-// possibilities without restarting entire operation
-
-// user picks 2nd container
-// stores as destination variable
-// changes clicked2 to false so destination won't be changed
-// calls compare function
-function selectDestination () {
-  if (start === container1) {
-    if (clicked2 === true) {
-      container2.addEventListener('click', () => {
-        destination = container2
-        console.log('selected destination 12')
-        console.log(destination)
-        compare()
-        clicked2 = false
-      })
-      container3.addEventListener('click', () => {
-        destination = container3
-        console.log('selected destination 13')
-        console.log(destination)
-        compare()
-        clicked2 = false
-      })
-    }
-  } else if (start === container2) {
-    if (clicked2 === true) {
-      container1.addEventListener('click', () => {
-        destination = container1
-        console.log('selected destination 21')
-        console.log(destination)
-        compare()
-        clicked2 = false
-      })
-      container3.addEventListener('click', () => {
-        destination = container3
-        console.log('selected destination 23')
-        console.log(destination)
-        compare()
-        clicked2 = false
-      })
-    }
-  } else if (start === container3) {
-    if (clicked2 === true) {
-      container1.addEventListener('click', () => {
-        destination = container1
-        console.log('selected destination 31')
-        console.log(destination)
-        compare()
-        clicked2 = false
-      })
-      container2.addEventListener('click', () => {
-        destination = container2
-        console.log('selected destination 32')
-        console.log(destination)
-        compare()
-        clicked2 = false
-      })
-    }
-  }
+// when second container clicked, stores destination in variables declared at beginning
+// removes event listeners to prevent JS restarting
+function selectDestination1 () {
+  destination = container1
+  container1.removeEventListener('click', selectDestination1)
+  container3.removeEventListener('click', selectDestination3)
+  compare()
+}
+function selectDestination2 () {
+  destination = container2
+  container2.removeEventListener('click', selectDestination2)
+  container3.removeEventListener('click', selectDestination3)
+  compare()
+}
+function selectDestination3 () {
+  destination = container3
+  container1.removeEventListener('click', selectDestination1)
+  container2.removeEventListener('click', selectDestination2)
+  compare()
 }
 
-// remove Child doesn't work if not original parent container
-// says child node does not exist even though it is
-// visible in new container
-// Plus selected stays as last block clicked from original container
+// sees if destination container has blocks
+// if container has blocks, compares sizes
 function compare () {
   if (destination.hasChildNodes() === true) {
-    if (selected.offsetWidth < destination.firstChild.offsetWidth) {
-      addChildOnTop()
-      console.log('child nodes' + selected.offsetWidth)
-      console.log('desti child nodes' + destination.firstChild.offsetWidth)
-      startPart2()
-    } else {
-      startGame()
-        console.log(container1)
-        console.log(selected) //why it only saves the last child from original parent container
-    }
+    console.log(destination.hasChildNodes())
+    compareChildren()
+    addChildOnTop()
   } else {
-    console.log('no child here')
-    addChild()
-    startPart2()
+    addChildOnTop()
   }
 }
 
+// @@@@@@@@@@@@@@@@@@broken. Doesn't register width
+function compareChildren () {
+  if (selected === block[1]) {
+    addChildOnTop()
+  } else {
+    startGame()
+  }
+}
+
+// adds children in container
 function addChildOnTop () {
   start.removeChild(selected)
   destination.prepend(selected)
-  console.log('child on Top')
-  clicked = true
-  clicked2 = true
-  console.log(selected)
+  start = null
+  destination = null
+  selected = null
   trackMoves()
 }
 
-function addChild () {
-  start.removeChild(selected)
-      console.log(start)
-      console.log(selected)
-  destination.appendChild(selected)
-      console.log(destination)
-      console.log('just put in END@@@@@@@@')
-  clicked = true
-  clicked2 = true
-  trackMoves()
-}
-
+// increases moves counter when container adds child
 function trackMoves () {
   movesStart++
-  moves.innerHTML = `${movesStart}`
+  moves.innerHTML = `Moves: ${movesStart}`
+  win()
+}
+
+// increases win counter when player moves all blocks to container 3
+function win () {
+  if (container3.childElementCount === 5) {
+    winText()
+    winCounter += 1
+    button.innerHTML = `Wins: ${winCounter}`
+  } else {
+    startGame()
+  }
+}
+
+// win popup text
+var popup = document.querySelector('.youwin')
+function winText () {
+  popup.classList.toggle('show')
+  setTimeout(() => {
+    popup.innerHTML = 'Play again?'
+  }, 1500)
+  popup.addEventListener('click', () => {
+    resetGame()
+  })
+}
+
+// resets game/move counter and restarts game
+function resetGame () {
+  if (container3.childElementCount !== 0) {
+    container3.removeChild(container3.firstChild)
+    container1.appendChild(block[0])
+    container1.appendChild(block[1])
+    container1.appendChild(block[2])
+    container1.appendChild(block[3])
+    container1.appendChild(block[4])
+    movesStart = 0
+    moves.innerHTML = `Moves: 0`
+  }
+  popup.classList.toggle('show')
+  startGame()
 }
 
 
-// @@@@ WON'T ALLOW CONTAINER ONE TO BE CHOSEN
+
+
+
+
+
+// all below is just some silly code
+// pressing win ups win count but ...
+var popupCheat = document.querySelector('.cheating')
+button.onclick = () => {
+  winCounter += 1
+  button.innerHTML = `Wins: ${winCounter}`
+  if (winCounter === 7) {
+    popupCheat.classList.toggle('show1')
+  }
+  if (winCounter === 16) {
+    popupCheat.innerHTML = `Really? You're still here?`
+  }
+  if (winCounter === 20) {
+    popupCheat.innerHTML = `Stop clicking!!`
+  }
+  if (winCounter === 25) {
+    popupCheat.innerHTML = `Seriously?`
+  }
+  if (winCounter === 28) {
+    popupCheat.innerHTML = `Stop!!`
+  }
+  if (winCounter === 31) {
+    popupCheat.innerHTML = `...`
+  }
+  if (winCounter === 35) {
+    popupCheat.innerHTML = `I'm warning you...`
+  }
+  if (winCounter === 48) {
+    popupCheat.innerHTML = `OK.`
+  }
+  if (winCounter === 50) {
+    popupCheat.innerHTML = `YOU DID THIS.`
+  }
+  if (winCounter === 53) {
+    document.body.style.visibility = 'hidden'
+    popupCheat.innerHTML = `You broke the game. -_-`
+    document.body.style.backgroundImage = "url('http://78.media.tumblr.com/tumblr_mab652PAHK1rf18ygo1_400.gif')"
+  }
+}
+
+var h1 = document.querySelector('h1')
+var instructions = document.querySelector('.instructions')
+var rules = document.querySelector('.rules')
+var blocks = document.querySelectorAll('.block')
+
+// meow mode
+// changes blocks into stacks of Nyan cats and texts to meows
+var meow = document.querySelector('.meow')
+meow.addEventListener('click', () => {
+  for (var i = 0; i < blocks.length; i++) {
+    blocks[i].classList.add('cat')
+    document.body.style.backgroundImage = "url('https://vignette.wikia.nocookie.net/clashofclans/images/3/30/NYAN_CAT.gif/revision/latest/scale-to-width-down/640?cb=20150415221840')"
+    document.body.style.backgroundRepeat = 'no-repeat'
+    document.body.style.backgroundSize = '400px'
+    h1.innerHTML = 'Meower of Catnoi'
+    rules.innerHTML = 'meow meow meow meow meow'
+    instructions.innerHTML = 'meow meow meow meow meow meow meow meow meow meow mewo meow meow meow meow meow meow meow'
+  }
+})
+
+// hard mode
+// makes blocks "hidden," changes text, and automatically increases move counter
+var hardMode = document.querySelector('.hardmode')
+hardMode.addEventListener('click', () => {
+  for (var i = 0; i < blocks.length; i++) {
+    document.querySelectorAll('.bg')[i].style.visibility = 'hidden'
+  }
+  h1.innerHTML = 'Cower in Hardnoi'
+  instructions.innerHTML = 'Objectives: Suffer. Rules: See objective.'
+  document.body.style.backgroundImage = 'none'
+  rules.innerHTML = 'Once you go black...'
+  document.body.style.backgroundImage = 'hidden'
+  setInterval(() => {
+    movesStart++
+    moves.innerHTML = `Moves: ${movesStart}`
+  }, 300)
+  setTimeout(() => {
+    winCounter++
+    document.querySelector('.button').style.visibility = 'visible'
+    document.querySelector('.button').innerHTML = `Loss: ${winCounter}`
+  }, 900)
+})
+
+// DON'T mode
+var scary = document.querySelector('.scary')
+scary.addEventListener('click', () => {
+  document.querySelector('.button').style.visibility = 'hidden'
+  document.body.style.visibility = 'hidden'
+  setTimeout(() => {
+    document.body.style.backgroundRepeat = 'no-repeat'
+    document.body.style.backgroundSize = 'stretch'
+    document.body.style.backgroundPosition = 'center'
+    document.body.style.backgroundImage = "url('https://pa1.narvii.com/6010/326945292c91404edb69eefb6b3ad68ebfc73eb1_hq.gif')"
+    setTimeout(() => {
+      document.body.style.background = 'red'
+      setTimeout(() => {
+        document.body.style.background = 'black'
+      }, 100)
+    }, 300)
+  }, 2000)
+})
