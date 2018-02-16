3d4680f72b9d6dbc200fabc4bf62a00835d05755
Nhi Nguyen <nnguy152@gmail.com>
2018-02-16 09:10:51 -0500

    .

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // selectors for containers/blocks
-var container1 = document.querySelector('.one')
-var container2 = document.querySelector('.two')
-var container3 = document.querySelector('.three')
+var container = document.querySelectorAll('.container')
 var block = document.querySelectorAll('.block')
 // stores first block on tower & first/second clicked container
 var selected = null
@@ -23,76 +21,76 @@ function startGame () {
 
 // adds listeners to containers
 function addListeners () {
-  container1.addEventListener('click', containerOneClicked)
-  container2.addEventListener('click', containerTwoClicked)
-  container3.addEventListener('click', containerThreeClicked)
+  container[0].addEventListener('click', containerOneClicked)
+  container[1].addEventListener('click', containerTwoClicked)
+  container[2].addEventListener('click', containerThreeClicked)
 }
 
 // when specific container clicked, goes to specific function
 // containerClicked function stores container/firstChild in variables declared at beginning
 // changes listener event functions to selectDestination
 function containerOneClicked () {
-  selected = container1.firstElementChild
+  selected = container[0].firstElementChild
   selected.style.border = '2px solid red'
-  start = container1
+  start = container[0]
   if (start.childElementCount === 0) {
     startGame()
   } else {
     removeListener()
-    container2.addEventListener('click', selectDestination2)
-    container3.addEventListener('click', selectDestination3)
+    container[1].addEventListener('click', selectDestination2)
+    container[2].addEventListener('click', selectDestination3)
   }
 }
 function containerTwoClicked () {
-  selected = container2.firstElementChild
+  selected = container[1].firstElementChild
   selected.style.border = '2px solid red'
-  start = container2
+  start = container[1]
   if (start.childElementCount === 0) {
     startGame()
   } else {
     removeListener()
-    container1.addEventListener('click', selectDestination1)
-    container3.addEventListener('click', selectDestination3)
+    container[0].addEventListener('click', selectDestination1)
+    container[2].addEventListener('click', selectDestination3)
   }
 }
 function containerThreeClicked () {
-  selected = container3.firstElementChild
+  selected = container[2].firstElementChild
   selected.style.border = '2px solid red'
-  start = container3
+  start = container[2]
   if (start.childElementCount === 0) {
     startGame()
   } else {
     removeListener()
-    container1.addEventListener('click', selectDestination1)
-    container2.addEventListener('click', selectDestination2)
+    container[0].addEventListener('click', selectDestination1)
+    container[1].addEventListener('click', selectDestination2)
   }
 }
 
 // removes Listeners to prevent restarting from beginning
 function removeListener () {
-  container1.removeEventListener('click', containerOneClicked)
-  container2.removeEventListener('click', containerTwoClicked)
-  container3.removeEventListener('click', containerThreeClicked)
+  container[0].removeEventListener('click', containerOneClicked)
+  container[1].removeEventListener('click', containerTwoClicked)
+  container[2].removeEventListener('click', containerThreeClicked)
 }
 
 // when second container clicked, stores destination in variables declared at beginning
 // removes event listeners to prevent JS restarting
 function selectDestination1 () {
-  destination = container1
-  container1.removeEventListener('click', selectDestination1)
-  container3.removeEventListener('click', selectDestination3)
+  destination = container[0]
+  container[0].removeEventListener('click', selectDestination1)
+  container[2].removeEventListener('click', selectDestination3)
   compare()
 }
 function selectDestination2 () {
-  destination = container2
-  container2.removeEventListener('click', selectDestination2)
-  container3.removeEventListener('click', selectDestination3)
+  destination = container[1]
+  container[1].removeEventListener('click', selectDestination2)
+  container[2].removeEventListener('click', selectDestination3)
   compare()
 }
 function selectDestination3 () {
-  destination = container3
-  container1.removeEventListener('click', selectDestination1)
-  container2.removeEventListener('click', selectDestination2)
+  destination = container[2]
+  container[0].removeEventListener('click', selectDestination1)
+  container[1].removeEventListener('click', selectDestination2)
   compare()
 }
 
@@ -165,7 +163,7 @@ function trackMoves () {
 
 // increases win counter when player moves all blocks to container 3
 function win () {
-  if (container1.childElementCount === 0 && container2.childElementCount === 0) {
+  if (container[0].childElementCount === 0 && container[1].childElementCount === 0) {
     winText()
     winCounter += 1
     button.innerHTML = `Wins: ${winCounter}`
@@ -188,13 +186,9 @@ function winText () {
 
 // resets game/move counter and restarts game
 function resetGame () {
-  if (container3.childElementCount !== 0) {
-    container3.removeChild(container3.firstChild)
-    container1.appendChild(block[0])
-    container1.appendChild(block[1])
-    container1.appendChild(block[2])
-    container1.appendChild(block[3])
-    container1.appendChild(block[4])
+  if (container[2].childElementCount !== 0) {
+    container[2].removeChild(container[2].firstChild)
+    container[0].appendChild(block[0], block[1], block[2], block[3], block[4])
     movesStart = 0
     moves.innerHTML = `Moves: 0`
   }
@@ -204,13 +198,13 @@ function resetGame () {
 
 // switch between easy & hard mode
 document.querySelector('.easy').addEventListener('click', () => {
-  container1.removeChild(block[3])
-  container1.removeChild(block[4])
+  container[0].removeChild(block[3])
+  container[0].removeChild(block[4])
 })
 
 document.querySelector('.hard').addEventListener('click', () => {
-  container1.appendChild(block[3])
-  container1.appendChild(block[4])
+  container[0].appendChild(block[3])
+  container[0].appendChild(block[4])
 })
 
 
